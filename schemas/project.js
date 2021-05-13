df79653bc7206ae9a5771a74017676bcf1bc9f04
Ralph Largo <devlargs@gmail.com>
2021-05-13 22:07:23 +0800

    initial commit

---
+++ schemas/project.js
@@ -0,0 +1,54 @@
+export default {
+  name: "project",
+  title: "Project",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name",
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "projectType",
+      title: "Project Type",
+      type: "reference",
+      to: { type: "projectType" },
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "websiteUrl",
+      title: "Website Url",
+      type: "url",
+      validation: (Rule) =>
+        Rule.required().uri({
+          scheme: ["http", "https", "mailto", "tel"],
+        }),
+    },
+  ],
+  preview: {
+    select: {
+      title: "name",
+      media: "image",
+    },
+  },
+};
---
+++ schemas/projectType.js
@@ -0,0 +1,26 @@
+export default {
+  name: "projectType",
+  title: "Project Type",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name",
+        maxLength: 96,
+      },
+    },
+  ],
+  preview: {
+    select: {
+      title: "name",
+    },
+  },
+};
