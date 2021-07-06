cb5d6d1a41c1e896ce8a7fd78ba5957269cbc332
Ralph Largo <devlargs@gmail.com>
2021-07-06 10:02:18 +0800

    added blogs schema

---
+++ schemas/blogs.js
@@ -0,0 +1,51 @@
+export default {
+  name: "blogs",
+  title: "Blogs",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "notionId",
+      title: "Notion ID",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          title: "Alternative Text",
+          type: "string",
+          options: {
+            isHighlighted: true,
+          },
+          validation: (Rule) => Rule.required(),
+        },
+      ],
+      validation: (Rule) => Rule.required(),
+    },
+  ],
+  preview: {
+    select: {
+      title: "name",
+      media: "image",
+    },
+  },
+};
--- schemas/schema.js
+++ schemas/schema.js
@@ -1,14 +1,13 @@
 // First, we must import the schema creator
-import createSchema from "part:@sanity/base/schema-creator";
-
 // Then import schema types from any plugins that might expose them
 import schemaTypes from "all:part:@sanity/base/schema-type";
-
+import createSchema from "part:@sanity/base/schema-creator";
+import author from "./author";
 // We import object and document schemas
 import blockContent from "./blockContent";
+import blogs from "./blogs";
 import category from "./category";
 import post from "./post";
-import author from "./author";
 import project from "./project";
 import projectType from "./projectType";
 import skills from "./skills";
@@ -31,5 +30,6 @@ export default createSchema({
     project,
     projectType,
     skills,
+    blogs
   ]),
 });
